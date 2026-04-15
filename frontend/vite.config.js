83512c2c9886c6a6656737106d1a4294c5f89736
Артём Фокин <thefantazzer@gmail.com>
2026-04-15 13:27:07 +0300

    fix: vite refused connection

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
+    server: {
+        host: true,
+        allowedHosts: ['anagrams.ru', 'www.anagrams.ru']
+    }
 })
