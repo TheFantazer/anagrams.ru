ea315d8c716f4870f279f427a3eb618a42d5d0b3
Артём Фокин <thefantazzer@gmail.com>
2026-04-18 17:20:16 +0300

    feat: unnecessary dependency + translations

--- frontend/src/locales/i18n.js
+++ frontend/src/locales/i18n.js
@@ -1,22 +1,13 @@
 import { createI18n } from "vue-i18n";
-import { useLangStore } from "@/stores/lang";
 
 import ru from "./ru.json";
 import en from "./en.json";
 
 const i18n = createI18n({
-    legacy: false,
-    locale: "ru",
-    fallbackLocale: "en",
-    messages: { ru, en }
+  legacy: false,
+  locale: "ru",
+  fallbackLocale: "en",
+  messages: { ru, en }
 });
-
-export function syncI18nWithStore(pinia) {
-    const langStore = useLangStore(pinia);
-
-    i18n.global.locale.value = langStore.lang;
-
-    langStore.setLang(i18n.global.locale.value);
-}
 
 export default i18n;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./locales/i18n";
+import { useUserStore } from "./stores/userStore";
 
 import "./assets/tokens.css";
 import "./assets/app.css";
@@ -11,9 +12,18 @@ import "./assets/game.css";
 import "./assets/pages.css";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+app.use(pinia);
 app.use(i18n);
 app.use(router);
+
+const userStore = useUserStore();
+i18n.global.locale.value = userStore.soloLang;
+
+import { watch } from "vue";
+watch(() => userStore.soloLang, (newLang) => {
+  i18n.global.locale.value = newLang;
+});
 
 app.mount("#app");
